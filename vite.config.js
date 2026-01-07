8fe6db5b7a33a86103cec101b6b940807d532af0
yanorenzo624 <yanorenzo624@gmail.com>
2026-01-07 09:39:38 -0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1'
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+  },
+})
