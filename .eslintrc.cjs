fe92e80f2a885081adc7b073c6c85b4c8d011d16
yanorenzo624 <yanorenzo624@gmail.com>
2026-01-09 10:55:15 -0800

    setup tailwind with conflict-safe class merging and used ESLint rules with react hooks with feature-based architecture.

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
